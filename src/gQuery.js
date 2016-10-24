65c490f9df002601ef313a0bbbcab699bd31b8a1
bigggge <me@haoduoyu.cc>
2016-10-24 10:40:46 +0800

    Add some methods

--- src/gQuery.js
+++ src/gQuery.js
@@ -3,17 +3,19 @@
  *
  * 参考 jQuery.js 和 zepto.js
  *
- * v 0.2.0
+ * v 0.2.1
  *
  * Created by xiepan on 2016/10/19 11:13.
  */
 
 //IIFE  立即执行函数表达式 模拟块作用域 立即执行 独立模块，防止命名冲突，解耦。
 (function (global, factory) {
-    // https://segmentfault.com/a/1190000003732752
-    // AMD 规范中，define 函数同样有一个公有属性 define.amd。
-    // AMD 中的参数便是这个模块的依赖。那么如何在 AMD 中提供接口呢？
-    // 它是返回一个对象，这个对象就作为这个模块的接口
+    /**
+     * @link https://segmentfault.com/a/1190000003732752
+     * AMD 规范中，define 函数同样有一个公有属性 define.amd。
+     * AMD 中的参数便是这个模块的依赖。那么如何在 AMD 中提供接口呢？
+     * 它是返回一个对象，这个对象就作为这个模块的接口
+     */
     if (typeof define === 'function' && define.amd) {
         define(function () {
             return factory(global)
@@ -25,8 +27,21 @@
 
     var VERSION = 'v0.2.0';
 
-    function logNoSupported(arg) {
+    /**
+     * @deprecated
+     */
+    function logNoSupportedOld(arg) {
         console.log(arg + ' is not supported in ' + VERSION);
+    }
+
+    /**
+     * 更优雅的 log
+     */
+    function logNoSupported() {
+        //将类似数组转换成数组，返回的结果是真正的Array，这样就可以应用Array下的所有方法了
+        var args = Array.prototype.slice.call(arguments);
+        args.push(' is not supported in ' + VERSION);
+        console.log.apply(console, args);
     }
 
     // IIFE http://weizhifeng.net/immediately-invoked-function-expression.html
@@ -38,9 +53,14 @@
             class2type = {},
             toString = class2type.toString,
             slice = emptyArray.slice,
-            //https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+            concat = emptyArray.concat,
+            /**
+             *  @link https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+             */
             isArray = Array.isArray,
-            simpleSelectorRE = /^[\w-]*$/;
+            simpleSelectorRE = /^[\w-]*$/,
+            readyRE = /complete|loaded|interactive/
+            ;
 
         // core.Z = function (dom, selector) {
         //     return new Z(dom, selector)
@@ -60,6 +80,11 @@
             dom.selector = selector || '';
             return dom;
         };
+
+        core.isZ = function (object) {
+            return object instanceof core.Z
+        };
+
 
         // 查询选择器
         core.querySelector = function (element, selector) {
@@ -128,9 +153,10 @@
             } : console.log("Error: Your browser not support '$.contains' method");
 
         /**
-         * https://segmentfault.com/a/1190000002670622
+         * @link https://segmentfault.com/a/1190000002670622
          * 如果参数obj是undefined或null，则通过String(obj)转换为对应的原始字符串“undefined”或“null”，
-         * 否则调用 class2type[toString.call(obj)]，使用Object的原型方法 toString()来获取obj的字符串表示，形式是 [object class]，
+         * 否则调用 class2type[toString.call(obj)]
+         * 使用Object的原型方法 toString()来获取obj的字符串表示，形式是 [object class]，
          * 然后从对象 class2type中取出[object class]对应的小写字符串并返回，未取到则返回“object
          * @param obj
          * @returns {*}
@@ -182,6 +208,15 @@
         }
 
         /**
+         * Flatten any nested arrays
+         * 优雅的数组降维
+         * @link http://www.cnblogs.com/front-end-ralph/p/4871332.html
+         */
+        function flatten(array) {
+            return array.length > 0 ? $.fn.concat.apply([], array) : array;
+        }
+
+        /**
          * 获取JavaScript 对象的类型。可能的类型有：
          * null undefined boolean number string function array date regexp object error
          */
@@ -230,13 +265,38 @@
         $.inArray = function (element, array, fromIndex) {
             return emptyArray.indexOf.call(array, element, fromIndex)
         };
-
+        /**
+         * 接受一个标准格式的 JSON 字符串，并返回解析后的 JavaScript 对象
+         */
+        $.parseJSON = JSON.parse;
 
         /**
          * 删除字符串首尾的空白符
          */
         $.trim = function (str) {
             return str == null ? "" : String.prototype.trim.call(str)
+        };
+
+        $.map = function (elements, callback) {
+            var value, values = [], i, key;
+            if (likeArray(elements)) {
+                for (i = 0; i < elements.length; i++) {
+                    value = callback(elements[i], i);
+                    if (value != null) {
+                        values.push(value);
+                    }
+                }
+            } else {
+                for (key in elements) {
+                    value = callback(elements[key], key);
+                    if (value != null) {
+                        values.push(value);
+                    }
+                }
+            }
+            console.log('Before flatten: ');
+            console.log(values);
+            return flatten(values);
         };
 
         $.each = function (elements, callback) {
@@ -265,7 +325,7 @@
 
         $.fn = {
             // constructor: core.Z,
-            // length: 0,
+            length: 0,
 
             // Because a collection acts like an array
             // copy over these useful array functions.
@@ -278,6 +338,38 @@
             size: function () {
                 return this.length
             },
+            ready: function (callback) {
+                if (readyRE.test(document.readyState) && document.body) {
+                    callback($);
+                } else {
+                    //js 高程p390
+                    document.addEventListener('DOMContentLoaded', function () {
+                        callback($)
+                    }, false);
+                }
+                return this;
+            },
+            get: function (idx) {
+                return idx === undefined ? slice.call(this) : this[idx >= 0 ? idx : idx + this.length]
+            },
+            toArray: function () {
+                return this.get()
+            },
+            concat: function () {
+                var i, value, args = [];
+                for (i = 0; i < arguments.length; i++) {
+                    value = arguments[i];
+                    args[i] = core.isZ(value) ? value.toArray() : value;
+                }
+                return concat.apply(core.isZ(this) ? this.toArray() : this, args)
+            },
+            map: function (fn) {
+
+            },
+            slice: function () {
+
+            }
+
         };
 
         // core.Z.prototype = Z.prototype = $.fn;
